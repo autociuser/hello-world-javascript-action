9973bd8c62fc4fb2aa3a9fae312e30adff161904
raghavharness <raghav.gupta@harness.io>
2023-08-03 18:33:12 +0530

    test

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ try {
   const pwd = execSync(`pwd`).toString().trim();
   console.log(`PWD ${pwd}`);
 
+  execSync(`cd /harness`)
+
   const ls = execSync(`ls -al`).toString().trim();
   console.log(`LS ${ls}`);
 

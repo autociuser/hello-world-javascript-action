e6ffaddaeca3bc82ff6e1930c1359f360a66f3d6
raghavharness <raghav.gupta@harness.io>
2023-08-03 17:09:42 +0530

    test

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import { execSync } from 'child_process';
+
+// const core = require('@actions/core');
+// const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const nameToGreet = core.getInput('who-to-greet');
+  // console.log(`Hello ${nameToGreet}!`);
+
+  const sha = execSync(`git rev-parse HEAD`).toString().trim();
+  console.log(`Sha ${sha}`);
+
+  // const time = (new Date()).toTimeString();
+  // core.setOutput("time", time);
+  // // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
-  core.setFailed(error.message);
+   console.log(error)
+  // core.setFailed(error.message);
 }

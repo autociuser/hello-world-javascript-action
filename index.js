eb6a6301393514439021b6e216772b194374eea3
raghavharness <raghav.gupta@harness.io>
2023-08-22 18:49:03 +0530

    changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // import { execSync } from 'child_process';
 
 const core = require('@actions/core');
-// const github = require('@actions/github');
 
 try {
   const repository = core.getInput('repository');
@@ -11,31 +10,43 @@ try {
   console.log(`Repo Owner: ${repository_owner}!`);
 
   const ref = core.getInput('ref');
-  console.log(`Repo Owner: ${ref}!`);
+  console.log(`Ref: ${ref}!`);
 
   const base_ref = core.getInput('base_ref');
-  console.log(`Repo Owner: ${base_ref}!`);
+  console.log(`Base Ref: ${base_ref}!`);
 
   const head_ref = core.getInput('head_ref');
-  console.log(`Repo Owner: ${head_ref}!`);
+  console.log(`Head Ref: ${head_ref}!`);
 
   const sha = core.getInput('sha');
-  console.log(`Repo Owner: ${sha}!`);
+  console.log(`Sha: ${sha}!`);
 
   const run_number = core.getInput('run_number');
-  console.log(`Repo Owner: ${run_number}!`);
+  console.log(`Run number: ${run_number}!`);
 
   const run_attempt = core.getInput('run_attempt');
-  console.log(`Repo Owner: ${run_attempt}!`);
+  console.log(`Run attempt: ${run_attempt}!`);
 
   const workspace = core.getInput('workspace');
-  console.log(`Repo Owner: ${workspace}!`);
+  console.log(`Workspace: ${workspace}!`);
 
   const ref_name = core.getInput('ref_name');
-  console.log(`Repo Owner: ${ref_name}!`);
+  console.log(`Ref Name: ${ref_name}!`);
 
   const ref_type = core.getInput('ref_type');
-  console.log(`Repo Owner: ${ref_type}!`);
+  console.log(`Ref type: ${ref_type}!`);
+
+  const arch = core.getInput('arch');
+  console.log(`Arch: ${arch}!`);
+
+  const OS = core.getInput('os');
+  console.log(`OS: ${OS}!`);
+
+  const runner_name = core.getInput('runner_name');
+  console.log(`Runner Name: ${runner_name}!`);
+
+  const runner_tool_cache = core.getInput('runner_tool_cache');
+  console.log(`Runner Tool Cache: ${runner_tool_cache}!`);
 
 } catch (error) {
   core.setFailed(error.message);
